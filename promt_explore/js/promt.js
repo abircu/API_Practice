59d081bf79b870b847b3b518a528530acac96915
abircu <mdabircse4873@gmail.com>
2023-09-05 00:13:09 +0600

    practice alert,promt, and confirm

---
+++ promt_explore/js/promt.js
@@ -0,0 +1,16 @@
+const moreAlert = () => {
+  alert("this alert work perfectly ");
+};
+const seriousStudent = () => {
+  const serious = confirm("please confirm your age");
+  console.log(serious);
+  if (serious === true) {
+    alert("very good you will success maybe");
+  } else {
+    alert("chup sala dukabaj.you are lazy person");
+  }
+};
+const PromtExplore = () => {
+  const studentName = prompt("what is your name");
+  console.log(studentName);
+};
